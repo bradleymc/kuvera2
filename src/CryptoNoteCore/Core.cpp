1a672a884746ca8254fd5350e80bb37ce1e98c9b
kuvera-project <35458115+kuvera-project@users.noreply.github.com>
2018-02-20 18:09:34 +1030

    Update Core.cpp

--- src/CryptoNoteCore/Core.cpp
+++ src/CryptoNoteCore/Core.cpp
@@ -434,10 +434,10 @@ bool Core::queryBlocksLite(const std::vector<Crypto::Hash>& knownBlockHashes, ui
     }
 
     fillQueryBlockShortInfo(fullOffset, currentIndex, BLOCKS_SYNCHRONIZING_DEFAULT_COUNT, entries);
-
-    return true;
-  } catch (std::exception&) {
-    // TODO log
+ 
+      return true;
+  } catch (std::exception& e) {
+	logger(Logging::ERROR) << "Failed to query blocks: " << e.what();
     return false;
   }
 }
